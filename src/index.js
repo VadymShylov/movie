a5ebf8f590cf5f039785262ceaeebb7fb9613843
VadymShylov <shylovvadym@gmail.com>
2023-06-18 22:45:09 +0300

    new

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider theme={theme}>
-    <BrowserRouter basename="/movie.git/">
+    <BrowserRouter basename="/movie/">
       <App />
       <ToastContainer autoClose={3000} position="top-center" />
     </BrowserRouter>
